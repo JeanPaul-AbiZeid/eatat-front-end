39d7e09f229b269575594ff2281d85bfeb935af0
Micho-PC <michelhdd27@gmail.com>
2022-05-28 19:01:26 +0300

    added redirects

--- pages/profile-page/scripts/profile_script.js
+++ pages/profile-page/scripts/profile_script.js
@@ -1,6 +1,12 @@
 const nav_home = document.getElementById("home");
+const rest_container = document.getElementById("rest-container");
 
 //when user clicks the home in header
 nav_home.addEventListener("click", function(){
     window.location.href = "../../pages/explore-page/explore.html"
+})
+
+//when user clicks the restaurant card
+rest_container.addEventListener("click",function(){
+    window.location.href = "../restaurant-page/restaurant.html";
 })
---
+++ pages/restaurant-page/scripts/restaurant_script.js
@@ -0,0 +1,6 @@
+const nav_home = document.getElementById("home");
+
+//when user clicks the home in header
+nav_home.addEventListener("click", function(){
+    window.location.href = "../../pages/explore-page/explore.html"
+})
