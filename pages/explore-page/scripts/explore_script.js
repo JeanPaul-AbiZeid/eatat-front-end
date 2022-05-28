92ea3578c8d0e80854bf35447af66ff51f3a5bc4
Micho-PC <michelhdd27@gmail.com>
2022-05-28 17:28:15 +0300

    added script for restaurant card

---
+++ pages/explore-page/scripts/explore_script.js
@@ -0,0 +1,6 @@
+const rest_container = document.getElementById("rest-container");
+
+//when user clicks the restaurant card
+rest_container.addEventListener("click",function(){
+    window.location.href = "../restaurant-page/restaurant.html";
+})
