01d21d58c0cedfcf105aa1a7724bf32473230047
Micho-PC <michelhdd27@gmail.com>
2022-05-28 17:49:01 +0300

    added home redirect

--- pages/explore-page/scripts/explore_script.js
+++ pages/explore-page/scripts/explore_script.js
@@ -1,6 +1,15 @@
 const rest_container = document.getElementById("rest-container");
+const nav_home = document.getElementById("home");
+
+//when user clicks the home in header
+nav_home.addEventListener("click", function(){
+    window.location.href = "./explore.html"
+})
 
 //when user clicks the restaurant card
 rest_container.addEventListener("click",function(){
     window.location.href = "../restaurant-page/restaurant.html";
 })
+
+
+
