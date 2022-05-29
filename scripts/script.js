45825339aa7adddc1ee32b8d584bdc9b7f80dc70
Micho-PC <michelhdd27@gmail.com>
2022-05-29 14:02:20 +0300

    cleaned code in script

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,16 @@
-/* //axios
-let data = new FormData();
-let id = 0;
-data.append('user_id', id);
-axios({
-    method: 'post',
-    url: 'http://localhost/project/get_restos.php',
-    data: data,
-    })
-.then(function (response) {
-    console.log(response);
-    })
- */
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("sign-up");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+//checkbox for showing password
 var show = document.getElementById("toggle");
+
+//function that shows password with checkbox
 show.addEventListener("click", reveal);
 function reveal() {
   var x = document.getElementById("myInput");
@@ -21,14 +20,6 @@ function reveal() {
     x.type = "password";
   }
 }
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("sign-up");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
 
 // When the user clicks the button, open the modal 
 btn.addEventListener("click", function(){
