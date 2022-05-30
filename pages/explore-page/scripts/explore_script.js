096124fcbac63888e4cf9479f7afd96fb990dc9f
Micho-PC <michelhdd27@gmail.com>
2022-05-30 20:44:45 +0300

    created fill page function

--- pages/explore-page/scripts/explore_script.js
+++ pages/explore-page/scripts/explore_script.js
@@ -1,10 +1,8 @@
-
-
-
+//linking restaurants data and creating resto cards accordingly
 axios({
     url: 'http://localhost:8080/eatAt-backend/eatat-backend/explore.php',
 }).then(function(response){
-    console.log(response.data); 
+    //console.log(response.data); 
     //looping over the array to get restaurant data
     for(let i=0; i<response.data.length; i++){
         let id = response.data[i]["id"];
@@ -22,7 +20,7 @@ axios({
     const rest_container = document.querySelectorAll(".rest-container");
 
     rest_container.forEach(function(item){
-        console.log("working");
+        //console.log("working");
         item.addEventListener("click",function(){
             //saved the clicked resto card id to local storage
             localStorage.setItem("clicked_resto_id", item.id);
@@ -61,7 +59,7 @@ function createResto(id,name,loc,cost,category,description,image){
     card_div.appendChild(info_div);
     info_div.className = "rest-info";
 
-    //creating resto name h3
+    //creating resto name h2
     const resto_name = document.createElement("h2");
     info_div.appendChild(resto_name);
     resto_name.innerText = name;
@@ -106,67 +104,4 @@ function createResto(id,name,loc,cost,category,description,image){
 
 document.getElementById("test").addEventListener('click',createResto);
 
-/* //creating restaurant card function
-function createResto(id,name,loc,cost,category,description,image){
-    //creating main div tag
-    const card_div = document.createElement("div");
-    document.body.appendChild(card_div);
-    card_div.id = id; //each card has its database id as html id
-    card_div.className = "rest-container";
-
-    //creating resto image and its div
-    const img_div = document.createElement("div");
-    card_div.appendChild(img_div);
-
-    const resto_img = document.createElement("img");
-    img_div.appendChild(resto_img);
-    resto_img.src = "./assets/rest-placeholder.png"; //TO BE CHAGED TO IMG FROM DATABASE
-
-    //creating div for info
-    const info_div = document.createElement("div");
-    card_div.appendChild(info_div);
-    info_div.className = "rest-info";
-
-    //creating resto name h3
-    const resto_name = document.createElement("h2");
-    info_div.appendChild(resto_name);
-    resto_name.innerText = name;
-
-    //creating div for location and loc icon
-    const loc_div = document.createElement("div");
-    info_div.appendChild(loc_div);
-
-    const loc_icon = document.createElement("img");
-    loc_icon.src = "./assets/icons8-location-24.png";
-    loc_div.appendChild(loc_icon);
-
-    const loc_name = document.createElement("h3");
-    loc_div.appendChild(loc_name);
-    loc_name.innerText = loc; //to be changed to location
-
-    //creating category h3
-    const categ = document.createElement("h3");
-    info_div.appendChild(categ);
-    categ.innerText = category; 
-
-    //creating avg price h3
-    const avg_price = document.createElement("h3");
-    info_div.appendChild(avg_price);
-    avg_price.innerText = cost; 
-
-    //creating rating div
-    const rating_div = document.createElement("div");
-    card_div.appendChild(rating_div);
-    rating_div.className = "rating";
-
-    const rating_h3 = document.createElement("h3");
-    rating_div.appendChild(rating_h3);
-    rating_h3.innerHTML = "3 &#11088"; //to be changed to rating??
-
-    //creating description p
-    const desc_p = document.createElement("p");
-    card_div.appendChild(desc_p);
-    desc_p.innerText = description; 
-
-} */
 
--- pages/restaurant-page/scripts/restaurant_script.js
+++ pages/restaurant-page/scripts/restaurant_script.js
@@ -1,6 +1,38 @@
 const nav_home = document.getElementById("home");
 
+//
+let url = "http://localhost:8080/eatAt-backend/eatat-backend/restaurant.php";
+let resto_id = localStorage.getItem("clicked_resto_id");
+url += "?id=" + resto_id;
+
+axios({
+    url: url,
+}).then(function(response){
+    console.log(response.data); 
+    //looping over the array to get restaurant data
+    let name = 
+        createResto(name,location,avg_cost,category,description);
+    
+})
+
+
 //when user clicks the home in header
 nav_home.addEventListener("click", function(){
     window.location.href = "../../pages/explore-page/explore.html"
 })
+
+function fillPage(name,loc,cost,category,description,image)
+{
+    //add resto info
+    const info_div = document.getElementById("info");
+    info_div.children[0].innerHTML = name;
+    info_div.children[1].innerHTML = loc;
+    info_div.children[0].innerHTML = cost;
+
+    //TODO add avg ratings andtotal ratings
+
+    //add desciption
+    document.getElementById("description").innerHTML = description;
+
+}
+
